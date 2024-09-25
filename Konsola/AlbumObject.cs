2de727a1a62aa8d1b53ce22159083f87a18d97da
pawel054 <pawel.marcisz1@gmail.com>
2024-09-25 08:56:58 +0200

    Add LoadDataFromFile function

--- Konsola/AlbumObject.cs
+++ Konsola/AlbumObject.cs
@@ -11,7 +11,17 @@ namespace Konsola
         public string Artist { get; set; }
         public string Album { get; set; }
         public int SongsNumber {  get; set; }
-        public DateTime Year { get; set; }
+        public int Year { get; set; }
         public int DownloadNumber {  get; set; }
+
+        public AlbumObject() { }
+        public AlbumObject(string artist, string album, int songsNumber, int year, int downloadNumber)
+        {
+            Artist = artist;
+            Album = album;
+            SongsNumber = songsNumber;
+            Year = year;
+            DownloadNumber = downloadNumber;
+        }
     }
 }
--- Konsola/Program.cs
+++ Konsola/Program.cs
@@ -2,9 +2,42 @@
 {
     internal class Program
     {
+        public static List<AlbumObject> albumData = new List<AlbumObject>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data.txt");
+            LoadDataFromFile(filePath);
+            DisplayData(albumData);
+        }
+
+        private static void LoadDataFromFile(string filePath)
+        {
+            List<string> data = new List<string>();
+            if (File.Exists(filePath))
+            {
+                var lines = File.ReadAllLines(filePath);
+                foreach (var line in lines)
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        data.Add(line);
+                    }
+                }
+
+                for(int i = 0; i < data.Count; i += 5)
+                {
+                    AlbumObject album = new AlbumObject
+                    {
+                        Artist = data[i],
+                        Album = data[i + 1],
+                        SongsNumber = int.Parse(data[i + 2]),
+                        Year = int.Parse(data[i + 3]),
+                        DownloadNumber = int.Parse(data[i+4])
+                    };
+
+                    albumData.Add(album);
+                }
+            }
         }
     }
 }
